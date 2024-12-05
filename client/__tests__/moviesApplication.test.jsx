efd3bbcfe5eebdc26c64ba9202f1245919095c29
thsletten <sletten.tom@gmail.com>
2024-12-05 11:53:45 +0100

    mange store endringer

--- client/__tests__/moviesApplication.test.jsx
+++ client/__tests__/moviesApplication.test.jsx
@@ -1,44 +1,15 @@
-import { MoviesApplication } from "../moviesApplication";
-import renderer from "react-test-renderer";
+import React from "react";
+import { create } from "react";
 import { MemoryRouter } from "react-router-dom";
-import { act } from "react-dom/test-utils";
+import { MoviesApplication } from "../moviesApplication";
 
-describe("movie application", () => {
-  it("renders front page", () => {
-    let component;
-    act(() => {
-      component = renderer.create(
-        <MemoryRouter>
-          <MoviesApplication />
-        </MemoryRouter>,
-      );
-    });
-    expect(component.toJSON()).toMatchSnapshot();
-  });
-
-  it("lists movies", async () => {
-    const mockFetchMovies = jest.fn().mockResolvedValue([
-      { id: 1, title: "Oppenheimer" },
-      { id: 2, title: "Barbie" },
-    ]);
-
-    let component;
-    await act(async () => {
-      component = renderer.create(
-        <MemoryRouter initialEntries={["/movies"]}>
-          <MoviesApplication fetchMovies={mockFetchMovies} />
-        </MemoryRouter>,
-      );
-    });
-
-    // Ensure the component has updated with the fetched movies
-    await act(async () => {
-      await Promise.resolve(); // Wait for any pending promises
-    });
-
-    expect(component.toJSON()).toMatchSnapshot();
-    expect(component.root.findByType("h2").children.join(" ")).toBe(
-      "Listing of all movies",
+describe("MoviesApplication component", () => {
+  it("renders without crashing", () => {
+    const component = create(
+      <MemoryRouter>
+        <MoviesApplication />
+      </MemoryRouter>,
     );
+    expect(component.toJSON()).toMatchSnapshot();
   });
 });
