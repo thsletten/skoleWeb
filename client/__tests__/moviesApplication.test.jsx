6029f22c1eee8225cff9c3069d61b3a702bb6a59
thsletten <sletten.tom@gmail.com>
2024-12-05 11:10:46 +0100

    fixed version

--- client/__tests__/moviesApplication.test.jsx
+++ client/__tests__/moviesApplication.test.jsx
@@ -1,34 +1,42 @@
 import { MoviesApplication } from "../moviesApplication";
-import renderer, { act } from "react-test-renderer";
+import renderer from "react-test-renderer";
 import { MemoryRouter } from "react-router-dom";
+import { act } from "react-dom/test-utils";
 
 describe("movie application", () => {
-  it("looks okay", () => {
-    const component = renderer.create(
-      <MemoryRouter>
-        <MoviesApplication />
-      </MemoryRouter>,
-    );
-    expect(component).toMatchSnapshot();
+  it("renders front page", () => {
+    let component;
+    act(() => {
+      component = renderer.create(
+        <MemoryRouter>
+          <MoviesApplication />
+        </MemoryRouter>
+      );
+    });
+    expect(component.toJSON()).toMatchSnapshot();
   });
 
   it("lists movies", async () => {
+    const mockFetchMovies = jest.fn().mockResolvedValue([
+      { id: 1, title: "Oppenheimer" },
+      { id: 2, title: "Barbie" },
+    ]);
+
     let component;
     await act(async () => {
       component = renderer.create(
         <MemoryRouter initialEntries={["/movies"]}>
-          <MoviesApplication
-            fetchMovies={() => [
-              { id: 1, title: "Oppenheimer" },
-              { id: 2, title: "Barbie" },
-            ]}
-          />
-        </MemoryRouter>,
+          <MoviesApplication fetchMovies={mockFetchMovies} />
+        </MemoryRouter>
       );
     });
-    expect(component).toMatchSnapshot();
-    expect(component.root.findByType("h2").children.join(" ")).toBe(
-      "Listing of all movies",
-    );
+
+    // Ensure the component has updated with the fetched movies
+    await act(async () => {
+      await Promise.resolve(); // Wait for any pending promises
+    });
+
+    expect(component.toJSON()).toMatchSnapshot();
+    expect(component.root.findByType("h2").children.join(" ")).toBe("Listing of all movies");
   });
 });
